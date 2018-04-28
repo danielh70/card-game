caf0d946548b488788f96a5f62d53da6f458c4bf
Daniel Hook <danhook007@icloud.com>
2018-04-28 09:05:04 -0700

    better logic

--- src/App.js
+++ src/App.js
@@ -13,20 +13,26 @@ class App extends Component {
 		}
 	}
 
+
+
 	componentWillMount() {
-		let currentState = Object.assign([], this.state.player1);
+		let currentStatePlayer1 = Object.assign([], this.state.player1);
+		let currentStateComputer = Object.assign([], this.state.computer);
 		let suits = [ "hearts", "spades", "diamonds", "clubs" ];
 		let numbers = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace" ];
 
 		for (var i = 0; i < numbers.length; i++) {
 			for (var j = 0; j < 4; j++) {
-				currentState.push({
+				currentStatePlayer1.push({
+					[numbers[i]]: suits[j]
+				})
+				currentStateComputer.push({
 					[numbers[i]]: suits[j]
 				})
 			}
 		}
 
-		this.setState({ player1: currentState, computer: currentState });
+		this.setState({ player1: currentStatePlayer1, computer: currentStateComputer });
 	}
 	//
 	// componentDidMount() {
@@ -54,7 +60,6 @@ class App extends Component {
 	// 	})
 	//
 	// }
-
 
 
 
--- src/components/blackjack.js
+++ src/components/blackjack.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Grid, Col } from 'react-bootstrap';
+import { Row, Grid, Col, Button } from 'react-bootstrap';
 import Loader from './loader';
 import '../css/main.css';
 // import '../css/App.css';
@@ -9,18 +9,56 @@ class Blackjack extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      computer: []
+      computer: [],
+      player1: [],
+      computerDeck: this.props.computer,
+      player1Deck: this.props.player1,
+      flipped: 'card',
+      status: true
     }
   }
 
-  componentWillMount() {
-    const { computer, player1 } = this.props
-    let cards = () => Math.floor(Math.random() * player1.length);
-    let compCard1 = computer[cards()];
-    let compCard2 = player1[cards()];
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   console.log("nexprops", nextProps, "prevstate", prevState);
+  // }
 
-    this.setState({ computer: [compCard1, compCard2]})
+  componentDidMount() {
+    const { computer, player1 } = this.props
+    const { computerDeck } = this.state
+    let deck = Object.assign({}, this.state, { player1Deck: player1, computerDeck: computer });
+    let computerCards = { ...this.state };
+    let cards = () => Math.floor(Math.random() * player1.length);
+
+    deck.computer.push(computer[cards()]);
+    deck.computer.push(computer[cards()]);
+
+    let removeFirst = deck.computerDeck.indexOf(this.state.computer[0])
+    deck.computerDeck.splice(removeFirst, 1);
+
+    let removeSecond = deck.computerDeck.indexOf(this.state.computer[1])
+    deck.computerDeck.splice(removeSecond, 1);
+
+    // console.log(removeFirst, removeSecond);
+
+    this.setState(deck)
   }
+
+
+  flipCard = () => {
+    this.setState({
+      flipped: 'card flipit'
+    });
+  }
+
+  drawCard = () => {
+    let state = Object.assign([], this.state.player1Deck)
+
+    state.splice(3, 1)
+    // console.log(state);
+
+    }
+
+
 
   render() {
     console.log("blackjack state", this.state);
@@ -31,22 +69,22 @@ class Blackjack extends Component {
         <Grid>
 
           <Row>
+            {this.state.computer.length &&
             <Col xs={4} xsOffset={4}>
               <h3>Computer:</h3>
 
               <div className="card-container">
-                <div className="card">
+                <a onClick={this.flipCard}>
+                <div className={this.state.flipped}>
                   <div className="front">
-
                     { Object.keys(computer[0]) } <br />
                     { Object.values(computer[0]) }
-
                   </div>
                   <div className="back">
                     <div className="photo"></div>
                   </div>
-
                 </div>
+                </a>
                 <div className="card" id="show">
                   <div className="front">
 
@@ -61,10 +99,16 @@ class Blackjack extends Component {
                 </div>
               </div>
             </Col>
+          }
           </Row>
 
           <Row>
-            <Col xs={4} xsOffset={4}>
+            <Col xs={2}>
+              <Button onClick={this.drawCard}>Hit</Button> <br /><br />
+              <Button>Stand</Button> <br /><br />
+              <Button>Double</Button> <br /><br />
+            </Col>
+            <Col xs={4} xsOffset={2}>
               <h3>Player One:</h3>
 
               <div className="card">
