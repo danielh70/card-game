13bdde97d4d8e90736b6cfd9060d3f688dec9967
Daniel Hook <danhook007@icloud.com>
2018-05-07 05:00:26 -0700

    oh boy here we go

--- card-game/src/index.js
+++ card-game/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Navbar from './components/NavBar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from "react-redux";
 import configureStore from "./configureStore";
